0bdf51f6d4240750e45cc0e36627b39755f56ba4
Meghana-Madala <70503388+Meghana-Madala@users.noreply.github.com>
2026-04-02 00:09:17 -0500

    Time: 4 ms (22.59%) | Memory: 20.7 MB (28.88%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,10 +3,20 @@ class Solution:
         n = len(nums)
         i = 0
         j = i+1
-        while(i<n and j < n):
-            while(j < n and nums[i] == nums[j]):
+        # while(i<n and j < n):
+        #     while(j < n and nums[i] == nums[j]):
+        #         j+=1
+        #     if j < n:
+        #         nums[i+1] = nums[j]
+        #         i+=1
+        #         j+=1
+        # return i+1
+        if n == 0:
+            return n
+        while (i <= j and j < n):
+            if (nums[i] == nums[j]):
                 j+=1
-            if j < n:
+            else:
                 nums[i+1] = nums[j]
                 i+=1
                 j+=1
