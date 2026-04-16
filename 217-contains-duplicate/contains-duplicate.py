a90add129485c0eca3bde115e823a5806d64cafd
Meghana-Madala <70503388+Meghana-Madala@users.noreply.github.com>
2026-04-16 00:19:49 -0500

    Time: 14 ms (43.40%) | Memory: 32.2 MB (47.99%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,5 +1,13 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        unique = set()
+        for i in nums:
+            if i in unique:
+                return True
+            else:
+                unique.add(i)
+        return False
+
         # total = sum(nums)
         # unique = set()
         # for i in nums:
@@ -9,7 +17,8 @@ class Solution:
         #     else:
         #         unique.add(diff)
         # return False
-        unique_nums = set(nums)
-        if len(nums) == len(unique_nums):
-            return False
-        return True
+
+        # unique_nums = set(nums)
+        # if len(nums) == len(unique_nums):
+        #     return False
+        # return True
