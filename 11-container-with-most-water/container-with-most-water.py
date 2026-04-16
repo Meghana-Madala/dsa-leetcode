dbd580a4393e91d933d7734798cb2a011a9ffd76
Meghana-Madala <70503388+Meghana-Madala@users.noreply.github.com>
2026-04-15 23:57:33 -0500

    Time: 44 ms (94.26%) | Memory: 30.2 MB (7.93%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -7,7 +7,10 @@ class Solution:
             water = max(water, (right-left)*min(height[left], height[right]))
             if(height[left] < height[right]):
                 left+=1
+            elif (height[left] > height[right]):
+                right-=1
             else:
+                left+=1
                 right-=1
         return water
         
