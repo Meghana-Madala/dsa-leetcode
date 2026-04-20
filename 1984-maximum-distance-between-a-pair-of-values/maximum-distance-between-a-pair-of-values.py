15664af04209ed54d388cfd0a3a79eb4bef14220
Meghana-Madala <70503388+Meghana-Madala@users.noreply.github.com>
2026-04-19 23:48:40 -0500

    Time: 27 ms (99.31%) | Memory: 36.2 MB (20.89%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.py
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.py
@@ -1,22 +1,28 @@
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
-        if nums1[len(nums1)-1] > nums2[0]:
-            return 0
-        max_dist = 0
-        for j in range(len(nums2)-1, -1, -1):
-            l=0
-            r=len(nums1)-1
-            while(l <= r):
-                mid = l + (r-l)//2
-                if(nums1[mid] <= nums2[j]):
-                    # if((mid-1 < 0) or (nums1[mid-1] > nums2[j])):
-                    #     return j-mid
-                    max_dist = max(max_dist, j-mid)
-                    r = mid-1
-                else:
-                    l = mid+1
+        
+        # max_dist = 0
+        # for j in range(len(nums2)-1, -1, -1):
+        #     l=0
+        #     r=len(nums1)-1
+        #     while(l <= r):
+        #         mid = l + (r-l)//2
+        #         if(nums1[mid] <= nums2[j]):
+        #             max_dist = max(max_dist, j-mid)
+        #             r = mid-1
+        #         else:
+        #             l = mid+1
                 
-                #mid = (l+r)//2
+        #         #mid = (l+r)//2
                 
-        return max_dist
+        # return max_dist
+        i=0
+        j=0
+        while(j<len(nums2)):
+            if(nums1[i] > nums2[j]):
+                i+=1
+            j+=1
+            if i >= len(nums1):
+                    break
+        return max(j-i-1,0)
                 
